10cd42c6bf2e8070704dabe3b11a86d0973d54ea
Sylvester Aswin <sylvester.aswin.stanley@zalando.de>
2017-06-05 01:17:52 +0200

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import request from 'request';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+);
+app.use(bodyParser.json());
+app.listen(process.env.PORT || 5000);
+
+//Server index page
+app.get('/', (req, res) => {
+  res.send('Deployed');
+});
+
+// Facebook webhook
+// User for verification
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.verify_token'] === 'this_is_my_token') {
+    console.log('Verified webhook');
+    res.status(200).send(req.query('hub.challange'));
+  } else {
+    console.error('Verification failed. The tokens do not match');
+    res.sendStatus(403);
+  }
+});
